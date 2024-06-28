df9f815cd326d02a17774296783db4b2ba99d7be
ilia543 <mrmainxpx@gmail.com>
2024-06-28 10:48:00 +0400

    first commit

---
+++ gamocda1/gamocda.py
@@ -0,0 +1,7 @@
+name = input("enter your name : ")
+email = input("enter yeur email : ")
+phone = input("enter your phone : ")
+password = input("enter your password : ")
+
+
+print(name + " " +  email + " " + phone + " " + password)
